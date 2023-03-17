998af2935107074fd32816c070728cd636a9817b
MukulDevPatel <patelmukuldev@gmail.com>
2023-03-17 17:17:09 +0530

    [Mukul Dev] Add: OOPs Problem Statement

---
+++ InventoryDataManagementProblem/InventoryOperation.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+
+namespace OOPsProblemStatement.InventoryDataManagementProblem
+{
+    public class InventoryOperation
+    {
+        public void ReadJsonFile(string filePath)
+        {
+            //Deserialization - Conversion of data from Json format to string format
+            //Serialization - Conversion of data from string format to Json format
+            var data = File.ReadAllText(filePath);
+            var result = JsonConvert.DeserializeObject<List<InventoryData>>(data);
+            foreach (var inventory in result)
+            {
+                Console.WriteLine(inventory.Name+"\t"+inventory.Weight+"\t"+inventory.PricePerKg+"\t"+inventory.Weight*inventory.PricePerKg);
+           }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using OOPsProblemStatement.InventoryDataManagementProblem;
+using System;
+namespace OOPsProblemStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           InventoryOperation inventory = new InventoryOperation();
+            inventory.ReadJsonFile(@"D:\BridgeLabz\Demo\OOPsProblemStatement\InventoryDataManagementProblem\Inventory.json");
+        }
+    }
+}
